ba1699c66e2386a22e0bd660f190486039633a67
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-29 14:09:11 +0200

    Ragionamento ex palindrome

---
+++ Palindrome ex/js/script.js
@@ -0,0 +1,13 @@
+/* Palidroma:
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma.
+Dare l’output relativo. */ 
+
+
+
+// 1.Chiedere all utente di inserire una parola
+
+
+// 2.capire se la parola è palindroma oppure no 
+
+// 3. Dare l output relativo 
