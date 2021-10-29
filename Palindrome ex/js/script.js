b20d8f152a451176941d5571af4d6bf47ed26a1d
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-29 15:30:54 +0200

    Indentazione e creata variabile parolaPalindroma

--- Palindrome ex/js/script.js
+++ Palindrome ex/js/script.js
@@ -8,30 +8,30 @@ Dare l’output relativo. */
 // 1.Chiedere all utente di inserire una parola
 let parola = prompt("Inserisci una parola");
 
-    function palindroma(parolaInserita){
-        // 2.capire se la parola è palindroma oppure no 
-        // 2.1 scompongo la parola 
-        let parolaScomposta = parolaInserita.split("");
+function palindroma(parolaInserita){
+    // 2.capire se la parola è palindroma oppure no 
+    // 2.1 scompongo la parola 
+    let parolaScomposta = parolaInserita.split("");
 
-        // 2.2 Inverto l ordine della parola scomposta
-        let inversoScomposto =  parolaScomposta.reverse()
+    // 2.2 Inverto l ordine della parola scomposta
+    let inversoScomposto =  parolaScomposta.reverse()
 
-        // 2.3 Ricompongo la parola 
-        let parolaRicomposta = inversoScomposto.join("");
-    
+    // 2.3 Ricompongo la parola 
+    let parolaRicomposta = inversoScomposto.join("");
 
-        // 3. Dare l output relativo 
-        // 3.1 se la parola  inversa è uguale alla parola  iniziale, allora la parola inserita è palinroma
-    
 
-        if ( parolaInserita === parolaRicomposta ) {
-            parolaPalindroma = "la parola inserita è palindroma";
-        } else{
-            parolaPalindroma = "la parola inserita non è palindroma"
-        }
-            return parolaPalindroma;
+    // 3. Dare l output relativo 
+    // 3.1 se la parola  inversa è uguale alla parola  iniziale, allora la parola inserita è palinroma
+
+    let parolaPalindroma;
+    if ( parolaInserita === parolaRicomposta ) {
+        parolaPalindroma = "la parola inserita è palindroma";
+    } else{
+        parolaPalindroma = "la parola inserita non è palindroma"
     }
+        return parolaPalindroma;
+}
 
-    // Richiamo la funzione
-    let veroFalso = palindroma(parola);
-    console.log(veroFalso);
+// Richiamo la funzione
+let veroFalso = palindroma(parola);
+console.log(veroFalso);
