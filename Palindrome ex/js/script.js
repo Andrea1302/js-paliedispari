1f0383dc4c430eb8fde2b7bfcfd813e352ae963b
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-29 15:39:17 +0200

    Commenti logici più sensati

--- Palindrome ex/js/script.js
+++ Palindrome ex/js/script.js
@@ -2,8 +2,9 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma.
 Dare l’output relativo. */ 
-
-
+// 1.Chiedere all utente di inserire una parola
+// 2.capire se la parola è palindroma oppure no 
+// 3. Dare l output relativo
 
 // 1.Chiedere all utente di inserire una parola
 let parola = prompt("Inserisci una parola");
@@ -20,8 +21,8 @@ function palindroma(parolaInserita){
     let parolaRicomposta = inversoScomposto.join("");
 
 
-    // 3. Dare l output relativo 
-    // 3.1 se la parola  inversa è uguale alla parola  iniziale, allora la parola inserita è palinroma
+    
+    // 2.4 se la parola  inversa è uguale alla parola  iniziale, allora la parola inserita è palinroma
 
     let parolaPalindroma;
     if ( parolaInserita === parolaRicomposta ) {
@@ -32,6 +33,7 @@ function palindroma(parolaInserita){
         return parolaPalindroma;
 }
 
+// 3. Dare l output relativo
 // Richiamo la funzione
 let veroFalso = palindroma(parola);
 console.log(veroFalso);
